948576b1918a0b0530dd6876279c2bf33ed73e97
sunguk0410 <kkcc4104@naver.com>
2024-10-01 20:03:25 +0900

    chapter9

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO article(id, title, content) VALUES(1, 'ga', '1111');
-INSERT INTO article(id, title, content) VALUES(2, 'na', '2222');
-INSERT INTO article(id, title, content) VALUES(3, 'da', '3333');
+INSERT INTO article(title, content) VALUES('ga', '1111');
+INSERT INTO article(title, content) VALUES('na', '2222');
+INSERT INTO article(title, content) VALUES('da', '3333');
