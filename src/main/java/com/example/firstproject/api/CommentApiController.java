48149c60c89007562b8d835671276afa73b69c37
sunguk0410 <kkcc4104@naver.com>
2024-10-05 17:49:22 +0900

    chapter15

--- src/main/java/com/example/firstproject/api/CommentApiController.java
+++ src/main/java/com/example/firstproject/api/CommentApiController.java
@@ -26,4 +26,16 @@ public class CommentApiController {
         CommentDto createdDto = commentService.create(articleId, dto);
         return ResponseEntity.status(HttpStatus.OK).body(createdDto);
     }
+
+    @PatchMapping("/api/comments/{id}")
+    public ResponseEntity<CommentDto> update(@PathVariable Long id, @RequestBody CommentDto dto) {
+        CommentDto updatedDto = commentService.update(id, dto);
+        return ResponseEntity.status(HttpStatus.OK).body(updatedDto);
+    }
+
+    @DeleteMapping("/api/comments/{id}")
+    public ResponseEntity<CommentDto> delete(@PathVariable Long id) {
+        CommentDto deletedDto = commentService.delete(id);
+        return ResponseEntity.status(HttpStatus.OK).body(deletedDto);
+    }
 }
