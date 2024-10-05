5e13e4c0a4c6f69ec842382493c57fdf549dc8e0
sunguk0410 <kkcc4104@naver.com>
2024-10-05 17:28:32 +0900

    chapter15 (오류 존재)

---
+++ src/main/java/com/example/firstproject/api/CommentApiController.java
@@ -0,0 +1,29 @@
+package com.example.firstproject.api;
+
+import com.example.firstproject.dto.CommentDto;
+import com.example.firstproject.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class CommentApiController {
+    @Autowired
+    private CommentService commentService;
+
+    @GetMapping("/api/articles/{articleId}/comments")
+    public ResponseEntity<List<CommentDto>> comments(@PathVariable Long articleId) {
+        // 서비스에 위임
+        List<CommentDto> dtos = commentService.comments(articleId);
+        return ResponseEntity.status(HttpStatus.OK).body(dtos);
+    }
+
+    @PostMapping("/api/articles/{articleId}/comments")
+    public ResponseEntity<CommentDto> create(@PathVariable Long articleId, @RequestBody CommentDto dto) {
+        CommentDto createdDto = commentService.create(articleId, dto);
+        return ResponseEntity.status(HttpStatus.OK).body(createdDto);
+    }
+}
